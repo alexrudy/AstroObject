d014863e866b865651ec92d8bc47950e30f9bdd3
Alex Rudy <alex.rudy@gmail.com>
2011-10-07 16:57:25 +0800

    Test uses a blackbody spectrum as example

--- test.py
+++ test.py
@@ -10,15 +10,16 @@
 execfile("__init__.py")
 import logging,os
 import Utilities
-from AstroImage import plt
-import AstroImage
+from AstroImage import plt,np
+import AstroImage, AstroSpectra
 
 
 LOG = logging.getLogger("TESTING")
+result = True
 
 FileName = "Tests/Hong-Kong.jpg"
 TestImage = AstroImage.FITSImage()
-
+LOG.info("== Image Tests Starting ==")
 LOG.info("Image Loading Procedure for File "+FileName+"...")
 TestImage.loadFromFile(FileName)
 LOG.info("Image Display:"+FileName+"...")
@@ -26,6 +27,7 @@ plt.figure(1)
 TestImage.show()
 LOG.info("Image Manipulation: GrayScale...")
 TestImage.save(TestImage.data()[:,:,1],"GrayScale")
+result = result and len(TestImage.object().shape) == 2
 LOG.info("Image Display: GrayScale...")
 plt.figure(2)
 TestImage.show()
@@ -41,10 +43,28 @@ TestImage.loadFromFITS("HongKong.fit")
 LOG.info("Image Display: Loaded from FITS")
 plt.figure(3)
 TestImage.show()
-plt.show()
+
+plt.draw()
+
+result = result and np.abs(TestImage.data()-TestImage.data("GrayScale")).max() < 1e-20
 
 if os.access("HongKong.fit",os.F_OK):
     LOG.debug("Removing HongKong.fit file")
     os.remove("HongKong.fit")
 
-LOG.info("TESTS PASSED")
+LOG.info("== Image Tests Complete ==")
+
+LOG.info("Result = %s" % ("Passed" if result else "Failed"))
+
+LOG.info("== Spectra Tests Starting ==")
+
+TestSpectra = AstroSpectra.FITSSpectra()
+
+LOG.info("Generating a Blackbody Spectrum at 5000K...")
+x = np.linspace(0.1e-6,2e-6,1000)[1:]
+TestSpectra.save(np.array([x,Utilities.BlackBody(x,5000)]),"BlackBody")
+LOG.info("Displaying a Spectrum")
+plt.figure(4)
+TestSpectra.showSpectrum()
+
+plt.show()
