fb5ab93930ec0b346ec5c3682628b83ebc8a97e2
Alex Rudy <alex.rudy@gmail.com>
2011-10-14 09:16:07 +0800

    Sepearted old and new test code

--- Test.py
+++ Test.py
@@ -44,6 +44,7 @@ class ObjectTests(unittest.TestCase):
         self.Object.save(self.EmptyFrame)
         self.assertRaises(TypeError,self.Object.save,[1,3])
         self.assertRaises(KeyError,self.Object.save,self.EmptyFrame)
+        
     
     def test_show(self):
         """FITSFrame will raise an AbstractError on FITSObject.show"""
@@ -52,6 +53,7 @@ class ObjectTests(unittest.TestCase):
         self.assertRaises(Utilities.AbstractError,self.Object.show)
         plt.savefig("Tests/simple_plot_gen")
         
+    
     def test_write(self):
         """Existance of written empty FITS File after calling FITSObject.write"""
         LOG.info("TEST: "+self.test_write.__doc__)
@@ -59,20 +61,23 @@ class ObjectTests(unittest.TestCase):
         self.Object.write()
         self.assertTrue(os.access,(self.EmptyFileName,os.F_OK))
         
+    
     def test_read(self):
         """Code Reads Empty FITS File error-free"""
         LOG.info("TEST: "+self.test_read.__doc__)
         self.Object.read(self.EmptyFileNameEx)
         
     
-    
     def tearDown(self):
         """Tears down UnitTests"""
         self.Object = None
         self.EmptyFrame = None
     
+
+
 class ImageTests(unittest.TestCase):
     """A class for testing the AstroImage objects"""
+    
     def setUp(self):
         """Sets up the Object Tests"""
         self.HongKongFileName = "Tests/Hong-Kong.fits"
@@ -84,11 +89,13 @@ class ImageTests(unittest.TestCase):
         self.EmptyObject = AstroImage.ImageObject()
         self.GrayScaleImage = np.int32(np.sum(mpimage.imread(self.HongKongImage),axis=2))
         
+    
     def test_loadfromfile(self):
         """ImageObject.loadFromFile method runs"""
         LOG.info("TEST: "+self.test_loadfromfile.__doc__)
         self.EmptyObject.loadFromFile(self.HongKongImage)
         
+    
     def test_manipulation(self):
         """ImageObject.save and ImageObject.data methods correctly handle two forms of data"""
         LOG.info("TEST: "+self.test_manipulation.__doc__)
@@ -97,6 +104,7 @@ class ImageTests(unittest.TestCase):
         self.assertTrue(len(self.EmptyObject.object().shape) == 2)
         self.EmptyObject.show()
         plt.savefig("Tests/grayscale_image_gen")
+        
     
     def test_frame(self):
         """AstroImage.ImageFrame saves Image Data"""
@@ -105,6 +113,7 @@ class ImageTests(unittest.TestCase):
         self.EmptyObject.save(frame)
         LOG.debug("Generated Frame %s" % frame)
         
+    
     def test_write(self):
         """ImageFrame produces a FITS File"""
         LOG.info("TEST: "+self.test_write.__doc__)
@@ -115,13 +124,14 @@ class ImageTests(unittest.TestCase):
         self.EmptyObject.write(self.HongKongFileName)
         self.assertTrue(os.access,(self.HongKongFileName,os.F_OK))
         
-        
+    
     def test_read(self):
         """ImageObject reads Image Files, but not Empty Files"""
         LOG.info("TEST: "+self.test_read.__doc__)
         self.EmptyObject.read(self.HongKongExFileName)
         self.assertRaises(ValueError,self.EmptyObject.read,self.EmptyFileName)
         
+    
     def test_readwrite(self):
         """Image Data is preserved through read and write"""
         LOG.info("TEST: "+self.test_readwrite.__doc__)
@@ -130,14 +140,15 @@ class ImageTests(unittest.TestCase):
         self.EmptyObject.read(self.TestReadWriteFileName,"Imported Hong Kong Image")
         self.assertAlmostEqual(np.abs(self.EmptyObject.data()-self.EmptyObject.data("GrayScale Hong Kong Image")).max(),0)
         
-        
+    
     def tearDown(self):
         """docstring for tearDown"""
         self.EmptyObject = None
         self.GrayScaleImage = None
         if os.access(self.TestReadWriteFileName,os.F_OK):
             os.remove(self.TestReadWriteFileName)
-
+        
+    
 
 if __name__ != '__main__':
     LOG.critical(__name__+" is not a module, do not run it as one!")
@@ -154,86 +165,3 @@ else:
     LOG.debug("Re-applying Console Handler...")
     LOG.info("Tests were %s" % "passed" if result.wasSuccessful() else "FAILED")
     
-    
-    
-    
-def ImageTests():
-    """Performs basic Image Tests"""
-
-    LOG.info("FITS File Writing...")
-    TestImage.FITS("HongKong.fit")
-    LOG.info("FITS File Reading...")
-    TestImage.loadFromFITS("HongKong.fit")
-
-    LOG.info("Image Display: Loaded from FITS")
-    plt.figure(3)
-    TestImage.show()
-    plt.title("Image"+TestImage.statename)
-
-    ImageResult = ImageResult and np.abs(TestImage.data()-TestImage.data("GrayScale")).max() < 1e-20
-
-    if os.access("HongKong.fit",os.F_OK):
-        LOG.debug("Removing HongKong.fit file")
-        os.remove("HongKong.fit")
-
-    LOG.info("== Image Tests Complete ==")
-
-    LOG.info("Result = %s" % ("Passed" if ImageResult else "Failed"))
-    return ImageResult
-
-
-def SpectraTests():
-    """Tests the facilities for raw spectra"""
-    LOG.info("== Spectra Tests Starting ==")
-
-    TestSpectra = AstroSpectra.SpectraObject()
-
-    LOG.info("Generating a Blackbody Spectrum at 5000K...")
-    x = np.linspace(0.1e-6,2e-6,1000)[1:]
-    TestSpectra.save(np.array([x,Utilities.BlackBody(x,5000)]),"BlackBody")
-    LOG.info("Displaying a Spectrum")
-    plt.figure(4)
-
-    TestSpectra.showSpectrum()
-    plt.xlabel("Wavelength")
-    plt.ylabel("Flux (Joules)")
-
-def AnalyticSpectraTests():
-    """docstring for AnalyticSpectraTests"""
-    LOG.info("== Analytic Spectra Testing ==")
-    TestSpectra = AstroSpectra.SpectraObject()
-    
-    LOG.info("Generating Spectrum Components")
-    BlackBody = AnalyticSpectra.BlackBodySpectrum(5000)
-    Gaussian = AnalyticSpectra.GaussianSpectrum(0.5e-6,0.5e-8,4e12)
-    Composed = Gaussian + BlackBody
-    x = np.linspace(0.1e-6,2e-6,1000)[1:]
-    LOG.info("Saving Generated Spectrum (and rendering...)")
-    TestSpectra.save(np.array([x,Composed(x)]),"Composed Spectrum")
-
-    LOG.info("Plotting Generated Spectrum")
-    plt.figure(5)
-    TestSpectra.showSpectrum()
-
-
-def IRAFTests():
-    """Testing IRAF Interaction"""
-    LOG.info("== IRAF Interaction Tests Starting ==")
-    FileName = "Tests/Hong-Kong.jpg"
-    TestImage = AstroImage.ImageObject()
-
-    LOG.info("Loading Image from File "+FileName+"...")
-    TestImage.loadFromFile(FileName)
-    
-    LOG.info("Image Manipulation: GrayScale...")
-    TestImage.save(TestImage.data()[:,:,1],"GrayScale")
-    
-    iraf.imarith(TestImage.inFITS(),"/",2,TestImage.outFITS(statename="Half"))
-    TestImage.reloadFITS()
-    plt.figure(6)
-    TestImage.show()
-
-
-
-
-# LOG.info("Result = %s" % ("Passed" if result else "Failed"))
