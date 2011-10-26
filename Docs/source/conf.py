9fd6fefd01bf90bd9e564ec508e1ccfdca4b9479
Alex Rudy <alex.rudy@gmail.com>
2011-10-26 14:36:33 +0800

    Version system is compatible with sphynx

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Alexander Rudy'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags.
 release = '0.2.0'
 
