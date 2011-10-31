92b7bcaabd36f859a6006adac785b4dfba8f441d
Alex Rudy <alex.rudy@gmail.com>
2011-10-31 21:47:34 +0800

    Tests for Analytic Spectra

--- Test_AstroObjectBase.py
+++ Test_AstroObjectBase.py
@@ -17,80 +17,98 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import os
 
-class test_FITSFrame(object):
-    """AstroObjectBase.FITSFrame"""
+__all__ = ['API_Abstract_Frame','API_Abstract_Object']
+
+class API_Abstract_Frame(object):
+    """Tests an Abstract Frame"""
         
+    def check_constants(self):
+        """API-Based Test Contains Appropriate Constants"""
+        passed = True
+        attributes = ['FRAME','DATA','INVALID','FRAMESTR']
+        for attribute in attributes:
+            passed &= hasattr(self,attribute)
+            
+            
+        assert passed
+    
     @nt.raises(AbstractError)
     def test_save(self):
         """Save to an abstract base class raises an AbstractError"""
-        AOB.FITSFrame.__save__(None,"None")
-
-
+        self.FRAME.__save__(self.DATA,"None")
+    
+    
+    @nt.raises(AbstractError)
+    def test_hdu(self):
+        """HDU method raises an AbstractError"""
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        HDU = BFrame.__hdu__()
+    
+    @nt.raises(AbstractError)
+    def test_primary_hdu(self):
+        """HDU primary raises an AbstractError"""
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        HDU = BFrame.__hdu__(primary=True)
+    
+    @nt.raises(AbstractError)
     def test_read_empty_HDU(self):
-        """Reads an empty HDU"""
+        """Reads an empty HDU raises an AbstractError"""
         HDU = pf.PrimaryHDU()
-        FFrame = AOB.FITSFrame.__read__(HDU,"Empty")
-        assert isinstance(FFrame,AOB.FITSFrame)
-        assert FFrame.label == "Empty"
-        
+        BFrame = self.FRAME.__read__(HDU,"Empty")
+        assert isinstance(BFrame,self.FRAME)
+        assert BFrame.label == "Empty"
+    
     @nt.raises(AbstractError)
     def test_read_image_HDU(self):
         """Read an ImageHDU fails"""
         HDU = pf.ImageHDU()
-        AOB.FITSFrame.__read__(HDU,"Empty")
+        self.FRAME.__read__(HDU,"Empty")
     
 
     @nt.raises(AbstractError)
     def test_read_non_empty_HDU(self):
         """Read a not empty HDU to an abstract base class fails"""
-        HDU = pf.PrimaryHDU(np.array([1,2,3]).astype(np.int16))
-        FFrame = AOB.FITSFrame.__read__(HDU,"Not Empty")
+        HDU = pf.PrimaryHDU(self.INVALID)
+        BFrame = self.FRAME.__read__(HDU,"Not Empty")
     
 
     @nt.raises(AbstractError)
     def test_call_should_fail(self):
         """Calling a base frame should raise an AbstractError"""
-        FFrame = AOB.FITSFrame("Empty")
-        assert FFrame.label == "Empty"
-        FFrame()
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        BFrame()
     
 
 
-    def test_hdu_generates_empty_hdu(self):
-        """Calling the HDU method should generate an empty HDU for primary and non-primary"""
-        FFrame = AOB.FITSFrame("Empty")
-        assert FFrame.label == "Empty"
-        HDU = FFrame.__hdu__()
-        assert isinstance(HDU,pf.ImageHDU)
-        assert HDU.data == None
-        HDU = FFrame.__hdu__(primary=True)
-        assert isinstance(HDU,pf.PrimaryHDU)
-        assert HDU.data == None
+
     
 
     @nt.raises(AbstractError)
     def test_show_should_fail(self):
         """Showing a base frame should fail"""
-        FFrame = AOB.FITSFrame("Empty")
-        assert FFrame.label == "Empty"
-        FFrame.__show__()
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        BFrame.__show__()
 
     def test_string_representation(self):
         """String representation"""
-        FFrame = AOB.FITSFrame("Empty")
-        assert FFrame.label == "Empty"
-        assert str(FFrame) == "<'FITSFrame' labeled 'Empty'>"
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        assert str(BFrame) == self.FRAMESTR
 
 
-class test_FITSObject(object):
-    """"AstroObjectBase.FITSObject"""
+class API_Abstract_Object(object):
+    """Tests an Abstract Object"""
     
     def setUp(self):
         """Fixture for setting up a basic image frame"""
         Image = np.ones((100,100))
         Image[45:55,45:55] = np.zeros((10,10))
-        self.Frame = AOB.FITSFrame("Empty")
-        self.FrameLabel = "Empty"
+        self.FRAME = self.FRAME("Empty")
+        self.FRAMELABEL = "Empty"
         self.HDU = pf.PrimaryHDU()
         self.imHDU = pf.ImageHDU()
         
@@ -98,41 +116,41 @@ class test_FITSObject(object):
     @nt.raises(TypeError)
     def test_save_with_data(self):
         """Saving to FITSObject fails with None data"""
-        FObject = AOB.FITSObject()
-        FObject.save(None)
+        BObject = self.OBJECT()
+        BObject.save(None)
         
     
     def test_save_with_object(self):
-        """Saving to FITSObject should succeed with FITSFrame"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        assert FObject.statename == self.FrameLabel
-        assert isinstance(FObject.object(),AOB.FITSFrame)
+        """Saving to FITSObject should succeed with FITSFRAME"""
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        assert BObject.statename == self.FRAMELABEL
+        assert isinstance(BObject.object(),self.FRAME)
     
     def test_save_object_with_label(self):
         """Saving an object with an explicit label should change that object's label"""
         NewLabel = "Other"
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame,NewLabel)
-        assert FObject.statename == NewLabel
-        assert FObject.object().label == NewLabel
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME,NewLabel)
+        assert BObject.statename == NewLabel
+        assert BObject.object().label == NewLabel
         
     
     def test_double_saving_an_object_should_reference(self):
         """Saving the same frame twice should cause referencing"""
         raise SkipTest("This is a bug, should be fixed in a later version.")
         NewLabel = "Other"
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.save(self.Frame,NewLabel)
-        assert FObject.statename == NewLabel
-        assert FObject.object().label == NewLabel
-        FObject.select(self.FrameLabel)
-        assert FObject.statename == self.FrameLabel
-        assert FObject.object().label == self.FrameLabel
-        FObject.select(NewLabel)
-        assert FObject.statename == NewLabel
-        assert FObject.object().label == NewLabel
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.save(self.FRAME,NewLabel)
+        assert BObject.statename == NewLabel
+        assert BObject.object().label == NewLabel
+        BObject.select(self.FRAMELABEL)
+        assert BObject.statename == self.FRAMELABEL
+        assert BObject.object().label == self.FRAMELABEL
+        BObject.select(NewLabel)
+        assert BObject.statename == NewLabel
+        assert BObject.object().label == NewLabel
         
     
     def test_write_and_read_with_empty_HDU(self):
@@ -140,14 +158,14 @@ class test_FITSObject(object):
         Filename = "TestFile.fits"
         if os.access(Filename,os.F_OK):
             os.remove(Filename)
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.write(Filename)
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.write(Filename)
         assert os.access(Filename,os.F_OK)
-        label = FObject.read(Filename)
+        label = BObject.read(Filename)
         assert label == [Filename]
-        assert Filename == FObject.statename
-        assert isinstance(FObject.object(),AOB.FITSFrame)
+        assert Filename == BObject.statename
+        assert isinstance(BObject.object(),self.FRAME)
         os.remove(Filename)
         
     
@@ -157,113 +175,144 @@ class test_FITSObject(object):
         Filename = "TestFile.fits"
         if os.access(Filename,os.F_OK):
             os.remove(Filename)
-        AObject = AOB.FITSObject()
+        AObject = self.OBJECT()
         AObject.read(Filename)
     
     def test_select_aquires_correct_state(self):
         """Select changes to correct state"""
         Label = "Other"
-        Frame = AOB.FITSFrame(Label)
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.save(Frame,Label)
-        assert FObject.statename == Label
-        assert FObject.object().label == Label
-        FObject.select(self.FrameLabel)
-        assert FObject.statename == self.FrameLabel
-        assert FObject.object().label == self.FrameLabel
+        FRAME = self.FRAME(Label)
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.save(FRAME,Label)
+        assert BObject.statename == Label
+        assert BObject.object().label == Label
+        BObject.select(self.FRAMELABEL)
+        assert BObject.statename == self.FRAMELABEL
+        assert BObject.object().label == self.FRAMELABEL
         
     
     @nt.raises(IndexError)
     def test_select_fails_on_unknown_state(self):
         """Select cannont select non-existant states"""
-        FObject = AOB.FITSObject()
-        FObject.select(self.FrameLabel)
+        BObject = self.OBJECT()
+        BObject.select(self.FRAMELABEL)
         
     
     @nt.raises(AbstractError)
     def test_data_raises_abstract_error(self):
         """Data should raise underlying abstract error with frame saved"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.data()
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.data()
         
     
     @nt.raises(KeyError)
     def test_data_raises_key_error(self):
         """Data should raise key error when no data is present"""
-        FObject = AOB.FITSObject()
-        FObject.data()
+        BObject = self.OBJECT()
+        BObject.data()
         
     
     @nt.raises(KeyError)
     def test_object_raises_key_error(self):
         """Object should raise key error when no data is present"""
-        FObject = AOB.FITSObject()
-        FObject.object()
+        BObject = self.OBJECT()
+        BObject.object()
         
     
     @nt.raises(KeyError)
     def test_cannot_duplicate_state_name(self):
         """Save should not allow duplication of state name"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.save(self.Frame)
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.save(self.FRAME)
     
     def test_list_statenames(self):
         """List should show all statenames"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame,"A")
-        FObject.save(self.Frame,"B")
-        assert ["A","B"] == FObject.list()
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME,"A")
+        BObject.save(self.FRAME,"B")
+        assert ["A","B"] == BObject.list()
         
     
     def test_list_should_show_no_statenames(self):
         """Empty object should return no statenames"""
-        FObject = AOB.FITSObject()
-        assert [] == FObject.list()
+        BObject = self.OBJECT()
+        assert [] == BObject.list()
         
     def test_remove_should_delete_state(self):
         """Remove Deletes States"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame,"A")
-        FObject.save(self.Frame,"B")
-        assert ["A","B"] == FObject.list()
-        FObject.remove("A")
-        assert ["B"] == FObject.list()
-        assert "A" not in FObject.states, "States: %s" % FObject.states
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME,"A")
+        BObject.save(self.FRAME,"B")
+        assert ["A","B"] == BObject.list()
+        BObject.remove("A")
+        assert ["B"] == BObject.list()
+        assert "A" not in BObject.states, "States: %s" % BObject.states
         
     @nt.raises(IndexError)
     def test_cannot_remove_nonexistant_state(self):
         """Cannot Remove Non-Existant State"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame,"A")
-        FObject.remove("B")
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME,"A")
+        BObject.remove("B")
         
     
     @nt.raises(KeyError)
     def test_show_should_raise_key_error(self):
         """Show should raise KeyError for an empty object"""
-        FObject = AOB.FITSObject()
-        FObject.show()
+        BObject = self.OBJECT()
+        BObject.show()
         
     @nt.raises(AbstractError)
     def test_show_should_raise_abstract_error_with_frame(self):
         """Show should call underlying show method, raising an abstract error"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.show()
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.show()
 
     @nt.raises(KeyError)
     def test_show_should_raise_key_error_with_wrong_statename(self):
         """Show should raise KeyError for a non-existent state name"""
-        FObject = AOB.FITSObject()
-        FObject.save(self.Frame)
-        FObject.show(self.FrameLabel + "JUNK...")
+        BObject = self.OBJECT()
+        BObject.save(self.FRAME)
+        BObject.show(self.FRAMELABEL + "JUNK...")
 
 
 
-
+class test_FITSFrame(API_Abstract_Frame):
+    """AstroObjectBase.FITSFrame"""
+    def setUp(self):
+        self.FRAME = AOB.FITSFrame
+        self.FRAMESTR = "<'FITSFrame' labeled 'Empty'>"
+        self.DATA = None
+        self.INVALID = np.array([1,2,3]).astype(np.int16)
+    
+        self.check_constants()
+    
+    def test_hdu(self):
+        """Calling the HDU method should generate an empty HDU for primary and non-primary"""
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        HDU = BFrame.__hdu__()
+        assert isinstance(HDU,pf.ImageHDU)
+        assert HDU.data == None
+        
+    def test_read_empty_HDU(self):
+        """Reads an empty HDU"""
+        HDU = pf.PrimaryHDU()
+        BFrame = self.FRAME.__read__(HDU,"Empty")
+        assert isinstance(BFrame,self.FRAME)
+        assert BFrame.label == "Empty"
+        
+    def test_primary_hdu(self):
+        """Generates an empty Primary HDU"""
+        BFrame = self.FRAME("Empty")
+        assert BFrame.label == "Empty"
+        HDU = BFrame.__hdu__(primary=True)
+        assert isinstance(HDU,pf.PrimaryHDU)
+        assert HDU.data == None
 
 
 
