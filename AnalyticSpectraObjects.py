ccb4939e6b512d9b7612b8441d9e9f9191a04669
Alex Rudy <alex.rudy@gmail.com>
2011-11-02 16:28:37 +0800

    Analytic Spectra Objects now includes a flat spectrum with a single value

--- AnalyticSpectraObjects.py
+++ AnalyticSpectraObjects.py
@@ -45,8 +45,21 @@ class GaussianSpectrum(AnalyticSpectra.AnalyticSpectrum):
         self.stdev = stdev
         self.height = height
         
+    
     def __call__(self,wavelength):
         """Calls a wavelength function"""
         return np.vstack((wavelength,Gaussian(wavelength,self.mean,self.stdev,self.height)))
         
+    
+
+class FlatSpectrum(AnalyticSpectra.AnalyticSpectrum):
+    """docstring for FlatSpectrum"""
+    def __init__(self, value, label=None):
+        if label == None:
+            label = "Flat spectrum with value %3f" % value
+        super(FlatSpectrum, self).__init__(label)
+        self.value = value
         
+    def __call__(self,wavelength):
+        """Calls a wavelength function"""
+        return np.vstack((wavelength,np.ones(wavelength.shape)*self.value))
