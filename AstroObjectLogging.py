79ccc2a9533101e2b108a3651a05477d4c3f9617
Alex Rudy <alex.rudy@gmail.com>
2011-12-15 01:14:16 +0800

    Rudimentary Logging Ability Functional
    - Logger is subclassed.
    - configure() method configures the settings
    - start() method actually applies those settings
    - messages caught before start() will all be dumped to the LOG file
    - custom levels and shortcuts for those levels have been included below the "DEBUG" level
    	- these low-level functions will notify the developer of library level logging items of interest.
    	- Normal level logging items should therefore be of interest to the end user

--- AstroObjectLogging.py
+++ AstroObjectLogging.py
@@ -12,52 +12,96 @@ import logging.handlers as handlers
 
 import math, copy, sys, time, os
 
-def update(d, u):
-    """A deep update command for dictionaries.
-    This is because the normal dictionary.update() command does not handle nested dictionaries."""
-    if len(u)==0:
-        return d
-    for k, v in u.iteritems():
-        if isinstance(v, collections.Mapping):
-            r = update(d.get(k, {}), v)
-            d[k] = r
-        else:
-            d[k] = u[k]
-    return d
+from Utilities import *
+
+logging.captureWarnings(True)
+
+levels = {"LIBDEBUG":2,"LIBINFO":5,"LIBWARN":8}
+for name,lvl in levels.iteritems():
+    logging.addLevelName(lvl,name)
+
+
 
 class LogManager(logging.getLoggerClass()):
     
+    def __init__(self,name):
+        super(LogManager,self).__init__(name)
+        self.configured = False
+        self.running = False
+        self.handling = False
+        self.level = False
+        self.initCustomLevels()
+        self.initialize()
+    
+    def initCustomLevels(self):
+        """Initializes the module level custom level functions as shortcuts"""
+        for name,lvl in levels.iteritems():
+            def log(*args):
+                self.log(lvl,*args)
+            setattr(self,name.lower(),log)
+    
     def initialize(self):
         """Initializes this logger to buffer"""
+        if self.handling:
+            raise ConfigurationError("Logger appears to be already handling messages")
+        if self.running:
+            raise ConfigurationError("Logger seems to already have started")
+        if self.configured:
+            raise ConfigurationError("Logger appears to be already configured")
+        
         self.buffer = handlers.MemoryHandler(1e6)
-        self.buffer.setLevel(logging.NOTSET)
-        logging.captureWarnings(True)
         self.addHandler(self.buffer)
+
         
-        self.level = logging.INFO
+        
+        self.libdebug("Logging Started")
+        self.running = True
     
-    def _configure(self):
+    config = {
+                'logging' : {
+                    'file' : {
+                        'enable' : True,
+                        'format' : "%(asctime)s : %(levelname)-8s : %(funcName)-20s : %(message)s",
+                        'dateformat' : "%Y-%m-%d-%H:%M:%S",
+                        'filename' : "AstroObject",
+                        'level' : None,
+                    },
+                    'console': {
+                        'enable' : True,
+                        'format' : "...%(message)s",
+                        'level' : None,
+                    },
+                },
+                'System' : {
+                    'Dirs': {
+                        'Logs' : "Logs/"
+                    },
+                },
+            }
+    
+    def configure(self,configFile=None,configuration=None):
         """Configure this logging object"""
-        self.config = {
-            'logging' : {
-                'file' : {
-                    'enable' : True,
-                    'format' : "%(asctime)s : %(levelname)-8s : %(funcName)-20s : %(message)s",
-                    'filename' : "SED",
-                },
-                'console': {
-                    'enable' : False,
-                    'format' : "...%(message)s"
-                },
-            },
-        }
+        if self.configured:
+            raise ConfigurationError("Logger appears to be already configured")
+        if self.handling:
+            raise ConfigurationError("Logger appears to be already handling")
+        # Configure from Variable
+        if configuration != None:
+            self.config = update(self.config,configuration)
+            self.debug("Updated Configuration from variable")            
+            self.configured |= True
+        # Configure from File
+        if configFile != None:
+            try:
+                with open(configFile,'r') as stream:
+                    loaded = yaml.load(stream)
+                    self.config = update(self.config,loaded)
+            except IOError as e:
+                self.warning("Couldn't load Configuration File %s" % self.configFileName)
+            self.configured |= True
         
-        try:
-            with open(self.configFileName,'r') as stream:
-                loaded = yaml.load(stream)
-                self.config = update(self.config,loaded)
-        except IOError as e:
-            self.log.warning("Couldn't load Configuration File %s" % self.configFileName)
+        if not self.configured:
+            self.libwarn("No configuration provided or accessed. Using defaults.")
     
     def start(self):
         """Starts this logger outputing"""
@@ -72,25 +116,34 @@ class LogManager(logging.getLoggerClass()):
         elif self.level:
             self.console.setLevel(self.level)
 
-        if self.config["System"]["logging"]["console"]["enable"]:
-            self.log.addHandler(self.console)
-            self.consolebuffer.setTarget(self.console)
+        if self.config["logging"]["console"]["enable"]:
+            self.addHandler(self.console)
         
-        self.consolebuffer.close()
-        self.log.removeHandler(self.consolebuffer)
-
         self.logfile = None
+        self.logfolder = self.config["System"]["Dirs"]["Logs"]
+        
         # Only set up the file log handler if we can actually access the folder
-        if os.access(self.config["System"]["Dirs"]["Logs"],os.F_OK) and self.config["System"]["logging"]["file"]["enable"]:
-            filename = self.config["System"]["Dirs"]["Logs"] + self.config["System"]["logging"]["file"]["filename"]+".log"
+        if os.access(self.logfolder,os.F_OK):
+            
+            filename = self.config["System"]["Dirs"]["Logs"] + self.config["logging"]["file"]["filename"]+".log"
+            
             self.logfile = logging.handlers.TimedRotatingFileHandler(filename=filename,when='midnight')
-            self.logfile.setLevel(logging.DEBUG)
-            fileformatter = logging.Formatter(self.config["System"]["logging"]["file"]["format"],datefmt="%Y-%m-%d-%H:%M:%S")
+            fileformatter = logging.Formatter(self.config["logging"]["file"]["format"],datefmt=self.config["logging"]["file"]["dateformat"])
             self.logfile.setFormatter(fileformatter)
-            self.log.addHandler(self.logfile)
-            # Finally, we should flush the old buffers
-            self.filebuffer.setTarget(self.logfile)
+            
+            if self.config["logging"]["file"]["level"]:
+                self.console.setLevel(self.config["logging"]["file"]["level"])
+            elif self.level:
+                self.console.setLevel(self.level)
+                        
+            if self.config["logging"]["file"]["enable"]:
+            
+                self.addHandler(self.logfile)
+                # Finally, we should flush the old buffers
+                self.buffer.setTarget(self.logfile)
 
-        self.filebuffer.close()
-        self.log.removeHandler(self.filebuffer)
-        self.log.debug("Configured Logging")
+        self.buffer.close()
+        self.removeHandler(self.buffer)
+        self.debug("Configured Logging")
+
+logging.setLoggerClass(LogManager)
