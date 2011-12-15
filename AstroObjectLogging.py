769bb717b4af557fc8e75655ebc3a951fd2d826a
Alex Rudy <alex.rudy@gmail.com>
2011-12-15 11:36:09 +0800

    New Logging system enabled
    - Connivence configuration
    - Ability to toggle console output
    - Management within the AstroObject Model
    - Optional module level logging.yaml configuration
    - Removed convinence functions for custom logging levels (they didn't preserve function names, nor function properly when set via iterators)

--- AstroObjectLogging.py
+++ AstroObjectLogging.py
@@ -29,16 +29,10 @@ class LogManager(logging.getLoggerClass()):
         self.configured = False
         self.running = False
         self.handling = False
+        self.doConsole = False
         self.level = False
-        self.initCustomLevels()
+        self.setLevel(1)
         self.initialize()
-    
-    def initCustomLevels(self):
-        """Initializes the module level custom level functions as shortcuts"""
-        for name,lvl in levels.iteritems():
-            def log(*args):
-                self.log(lvl,*args)
-            setattr(self,name.lower(),log)
     
     def initialize(self):
         """Initializes this logger to buffer"""
@@ -50,22 +44,22 @@ class LogManager(logging.getLoggerClass()):
             raise ConfigurationError("Logger appears to be already configured")
         
         self.buffer = handlers.MemoryHandler(1e6)
+        self.buffer.setLevel(0)
         self.addHandler(self.buffer)
-
-        self.libdebug("Logging Started")
+        
         self.running = True
     
     config = {
                 'logging' : {
                     'file' : {
                         'enable' : True,
-                        'format' : "%(asctime)s : %(levelname)-8s : %(funcName)-20s : %(message)s",
+                        'format' : "%(asctime)s : %(levelname)-8s : %(module)-40s : %(funcName)-10s : %(message)s",
                         'dateformat' : "%Y-%m-%d-%H:%M:%S",
                         'filename' : "AstroObject",
                         'level' : None,
                     },
                     'console': {
-                        'enable' : True,
+                        'enable' : False,
                         'format' : "...%(message)s",
                         'level' : None,
                     },
@@ -95,12 +89,12 @@ class LogManager(logging.getLoggerClass()):
                     loaded = yaml.load(stream)
                     self.config = update(self.config,loaded)
             except IOError as e:
-                self.warning("Couldn't load Configuration File %s" % self.configFileName)
+                self.warning("Couldn't load Configuration File %s" % configFile)
             else:
                 self.configured |= True
         
         if not self.configured:
-            self.libwarn("No configuration provided or accessed. Using defaults.")
+            self.log(8,"No configuration provided or accessed. Using defaults.")
     
     def start(self):
         """Starts this logger outputing"""
@@ -109,7 +103,7 @@ class LogManager(logging.getLoggerClass()):
         if not self.running:
             raise ConfigurationError("Logger appears to not be running. This should never happen")
         if not self.configured:
-            self.libwarn("Logger appears not to be configured")
+            self.log(8,"Logger appears not to be configured")
         
         # Setup the Console Log Handler
         self.console = logging.StreamHandler()    
@@ -121,7 +115,7 @@ class LogManager(logging.getLoggerClass()):
         elif self.level:
             self.console.setLevel(self.level)
 
-        if self.config["logging"]["console"]["enable"]:
+        if self.config["logging"]["console"]["enable"] and not self.doConsole:
             self.addHandler(self.console)
             self.handling |= True
         
@@ -130,7 +124,6 @@ class LogManager(logging.getLoggerClass()):
         
         # Only set up the file log handler if we can actually access the folder
         if os.access(self.logfolder,os.F_OK):
-            
             filename = self.config["System"]["Dirs"]["Logs"] + self.config["logging"]["file"]["filename"]+".log"
             
             self.logfile = logging.handlers.TimedRotatingFileHandler(filename=filename,when='midnight')
@@ -138,9 +131,9 @@ class LogManager(logging.getLoggerClass()):
             self.logfile.setFormatter(fileformatter)
             
             if self.config["logging"]["file"]["level"]:
-                self.console.setLevel(self.config["logging"]["file"]["level"])
+                self.logfile.setLevel(self.config["logging"]["file"]["level"])
             elif self.level:
-                self.console.setLevel(self.level)
+                self.logfile.setLevel(self.level)
                         
             if self.config["logging"]["file"]["enable"]:
             
@@ -150,8 +143,25 @@ class LogManager(logging.getLoggerClass()):
                 self.handling |= True
         
         self.removeHandler(self.buffer)
-        self.buffer.close()
-        if self.handling:
-            self.libdebug("Configured Logging")
+        self.buffer.flush()
+        if not self.handling:
+            raise ConfigurationError("Logger not actually handling anything!")
+            
+    def toggleConsole(self,value=None):
+        """Turn on or off the console logging"""
+        if value != None:
+            self.doConsole = not value
+        if not self.handling:
+            raise ConfigurationError("Logger appears to be already handling messages")
+        if not self.running:
+            raise ConfigurationError("Logger appears to not be running. This should never happen")
+        if not self.config["logging"]["console"]["enable"]:
+            return
+        if self.doConsole:
+            self.removeHandler(self.console)
+            self.doConsole = False
+        else:
+            self.addHandler(self.console)
+            self.doConsole = True
 
 logging.setLoggerClass(LogManager)
