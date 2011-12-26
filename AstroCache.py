be003fc75dbe62568d8aaa5bf0d3430d3f1f7712
Alex Rudy <alex.rudy@gmail.com>
2011-12-26 23:56:40 +0800

    Caching can be enabled and disabled, and passes streams around for saving etc.

--- AstroCache.py
+++ AstroCache.py
@@ -40,48 +40,110 @@ class CacheStateError(CacheError):
 
 class Cache(object):
     """A simple cache object"""
-    def __init__(self,name=None,generate=None,load=None,stage=None,**kwargs):
+    def __init__(self,name=None,generate=None,load=None,save=None,stage=None,autosave=True,enabled=True,filename=None,directory=None,**kwargs):
         super(Cache, self).__init__()
         self.name = name
         self.cache_generate = generate
+        self.cache_save = save
         self.cache_load = load
         self.stage = stage
+        self.autosave = autosave
+        self.enabled = enabled
+        
+        if filename == None:
+            filename = self.name.replace(" ","")
+        self.filename = os.path.basename(filename)
+        if filename == "":
+            raise CacheError("Filename is invalid: %s" % filename)
+        if not self.filename.endswith(".cache"):
+            self.filename += ".cache"
+        
+        if directory == None:
+            directory = "Caches"
+        if not os.path.isdir(directory):
+            raise CacheError("Specified directory is invalid: %s" % directory)
+        self.directory = directory
+        self.fullname = os.path.join(self.directory,self.filename)
+        
+        
+        # State Variables
         self.regenerate = False
         self.generated = False
         self.loaded = False
+        self.ready = False
+        self.saved = False
+        
         self.log = logging.getLogger(__name__)
-        self.data = False
+        
+        self.data = None
         
     def load(self):
         """Load the contents of this cache into the cache object, using the load function."""
-        if self.loaded:
-            raise CacheStateError("Cannot doubly load cache")
+        if self.ready:
+            return self.loaded
         try:
-            self.data = self.cache_load()
-        except CacheIOError as e:
+            with file(self.fullname,'r') as stream:
+                self.data = self.cache_load(stream)
+        except (IOError,CacheIOError) as e:
             self.cache_msg = str(e)
             self.log.debug("%(name)s Cache Load Failure. %(msg)s." % { 'name': self.name, 'msg': self.cache_msg })
             self.regenerate = True
         else:
             self.loaded = True
-            self.log.log(2,"%(name)s Cache Loaded" % { 'name': self.name } )
+            self.ready = True
+            self.log.log(2,"%(name)s Cache data loaded" % { 'name': self.name } )
         return self.loaded
         
-    def generate(self):
-        """Generate the cache files."""
-        if not self.generated and self.regenerate:
+    def generate(self,save=True):
+        """Generate the cache data."""
+        if self.ready:
+            return self.generated
+        if self.regenerate:
             try:
                 self.data = self.cache_generate()
-            except CacheIOError as e:
+            except (IOError,CacheError) as e:
+                self.cache_msg = str(e)
+                self.log.critical("%(name)s Cache Generate Failure. %(msg)s." % { 'name': self.name, 'msg': self.cache_msg })
+                raise
+            else:
+                self.log.debug("%(name)s generated Cache data." % { 'name': self.name })
+                self.regenerate = False
+                self.generated = True
+                self.ready = True
+        if self.autosave and save and self.enabled:
+            self.generated &= self.save()
+        return self.generated
+    
+    def save(self):
+        """Save the cache files"""
+        if not self.enabled or not self.ready:
+            return False
+        if self.ready and not self.regenerate:
+            try:
+                with file(self.fullname,'w') as stream:
+                    self.cache_save(stream,self.data)
+            except (IOError,CacheIOError) as e:
                 self.cache_msg = str(e)
                 self.log.critical("%(name)s Cache Save Failure. %(msg)s." % { 'name': self.name, 'msg': self.cache_msg })
                 raise
             else:
-                self.log.debug("%(name)s generated Cache Files." % { 'name': self.name })
-                self.regenerate = False
-                self.generated = True
-                self.loaded = True
-        return self.generated
+                self.log.debug("%(name)s saved Cache Files." % { 'name': self.name })
+                self.saved = True
+        else:
+            raise CacheStateError("Data needs to be %s" % ("regenerated" if self.regenerate else "generated"))
+        return self.saved
+        
+    def clear(self):
+        """Clear the cache file"""
+        if self.loaded:
+            self.data = None
+            self.loaded = False
+            self.ready = False
+        if self.saved:
+            self.saved = False
+        if os.access(self.fullname,os.W_OK):
+            os.remove(self.fullname)
+            self.log.debug("Removed File %s" % self.fullname)
     
     def __call__(self):
         """Call this cache to extract data"""
@@ -98,31 +160,26 @@ class Cache(object):
         
 class NumpyCache(Cache):
     """A cache of a numpy array"""
-    def __init__(self, directory = "", filename = "cache.npy", **kwargs):
+    def __init__(self, **kwargs):
         super(NumpyCache, self).__init__(**kwargs)
-        self.directory = directory
-        self.filename = filename
-        self.cache_gen = self.cache_generate
-        self.cache_get = self.cache_load
-        self.cache_generate = self.write
+        if self.cache_load != None:
+            raise CacheError("Function provided for LOAD in NPY cache... hmmm")
+        self.cache_save = self.write
         self.cache_load = self.read
-        self.fullname = os.path.join(self.directory,self.filename)
         
-    def write(self):
+    def write(self,stream,data):
         """Write a numpy file for this data item"""
-        if not self.generated and self.regenerate:
-            try:
-                self.data = self.cache_gen()
-                np.save(self.fullname,self.data)
-            except Exception as e:
-                self.cache_msg = str(e)
-                self.log.critical("%(name)s generating Cache Files: %(msg)s" % { 'name': self.name, 'msg': self.cache_msg })
-                raise
-            else:
-                self.log.debug("%(name)s generated Cache Files" % { 'name': self.name })
-                return self.data
+        try:
+            self.log.log(2,"Writing NPY Cache to %s" % self.fullname)
+            np.save(stream,data)
+        except Exception as e:
+            self.cache_msg = str(e)
+            self.log.critical("%(name)s writing Cache Files: %(msg)s" % { 'name': self.name, 'msg': self.cache_msg })
+            raise
+        else:
+            self.log.debug("%(name)s writing Cache Files" % { 'name': self.name })
     
-    def read(self):
+    def read(self,stream):
         """Read a numpy file for this data item"""
         try:
             self.data = np.load(self.fullname)
@@ -145,17 +202,19 @@ class CacheManager(object):
         """Register a Numpy Cache"""
         if generate == None:
             raise CacheError("Cache %s must specify generate function" % name)
-        cache = NumpyCache(name=name,generate=generate,load=lambda : None,stage=stage,directory=directory, filename=filename, **kwargs)
+        cache = NumpyCache(name=name,generate=generate,stage=stage,directory=directory, filename=filename, **kwargs)
         self.caches[name] = cache
         self.log.debug("Registered Cache %s" % name)
     
-    def register(self,name,generate=None,load=None,stage=None,directory=None):
+    def register(self,name,generate=None,load=None,save=None,stage=None,directory=None,**kwargs):
         """Register a new cache, which will then be managed by this cache manager"""
         if generate == None:
             raise CacheError("Cache %s must specify generate function" % name)
         if load == None:
             raise CacheError("Cache %s must specify load function" % name)
-        cache = Cache(name,generate,load,stage=stage,directory=directory)
+        if save == None:
+            raise CacheError("Cache %s must specify save function" % name)
+        cache = Cache(name,generate,load,save,stage=stage,directory=directory,**kwargs)
         self.caches[name] = cache
         self.log.debug("Registered Cache %s" % name)
     
@@ -197,6 +256,27 @@ class CacheManager(object):
                 loaded += [cache]
         return loaded
         
+    def save(self,*caches):
+        """Save the caches to files"""
+        saved = []
+        if len(caches) == 0:
+            caches = tuple(self.caches.keys())
+        for cache in caches:
+            if self.caches[cache].save():
+                saved += [cache]
+        return saved
+    
+    def clear(self,*caches):
+        """Save the caches to files"""
+        cleared = []
+        if len(caches) == 0:
+            caches = tuple(self.caches.keys())
+        for cache in caches:
+            if self.caches[cache].clear():
+                cleared += [cache]
+        return cleared
+    
+        
     def trigger(self,*caches):
         """Trigger the caches to regenerate"""
         triggered = []
@@ -206,10 +286,32 @@ class CacheManager(object):
             if self.caches[cache].trigger():
                 triggered += [cache]
         return triggered
-    
+        
+    def disable(self,*caches):
+        """Disable Caching"""
+        disabled = []
+        if len(caches) == 0:
+            caches = tuple(self.caches.keys())
+        for cache in caches:
+            if self.caches[cache].enabled:
+                disabled += [cache]
+            self.caches[cache].enabled = False
+        return disabled
+        
+    def enabled(self,*caches):
+        """Enable caching"""
+        enabled = []
+        if len(caches) == 0:
+            caches = tuple(self.caches.keys())
+        for cache in caches:
+            if self.caches[cache].enabled:
+                enabled += [cache]
+            self.caches[cache].enabled = True
+        return enabled
+        
     def get(self,cache):
         """Get a particular cache"""
-        if self.shouldGenerate(cache) or not ( enabled or self.caches[cache].generated ):
+        if self.shouldGenerate(cache):
             self.generate(cache)
         return self.caches[cache]()
         
