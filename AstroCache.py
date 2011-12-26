a720b8b209920210ca9b1e30d744eb38406ffe94
Alex Rudy <alex.rudy@gmail.com>
2011-12-26 13:53:57 +0800

    Added a simple NUMPY Data Simulator

--- AstroCache.py
+++ AstroCache.py
@@ -41,7 +41,7 @@ class CacheStateError(CacheError):
 class Cache(object):
     """A simple cache object"""
     def __init__(self,name=None,generate=None,load=None,stage=None,**kwargs):
-        super(Cache, self).__init__(**kwargs)
+        super(Cache, self).__init__()
         self.name = name
         self.cache_generate = generate
         self.cache_load = load
@@ -96,22 +96,66 @@ class Cache(object):
             return self.regenerate
         return False
         
+class NumpyCache(Cache):
+    """A cache of a numpy array"""
+    def __init__(self, directory = "", filename = "cache.npy", **kwargs):
+        super(NumpyCache, self).__init__(**kwargs)
+        self.directory = directory
+        self.filename = filename
+        self.cache_gen = self.cache_generate
+        self.cache_get = self.cache_load
+        self.cache_generate = self.write
+        self.cache_load = self.read
+        self.fullname = os.path.join(self.directory,self.filename)
+        
+    def write(self):
+        """Write a numpy file for this data item"""
+        if not self.generated and self.regenerate:
+            try:
+                self.data = self.cache_gen()
+                np.save(self.fullname,self.data)
+            except Exception as e:
+                self.cache_msg = str(e)
+                self.log.critical("%(name)s generating Cache Files: %(msg)s" % { 'name': self.name, 'msg': self.cache_msg })
+                raise
+            else:
+                self.log.debug("%(name)s generated Cache Files" % { 'name': self.name })
+                return self.data
     
+    def read(self):
+        """Read a numpy file for this data item"""
+        try:
+            self.data = np.load(self.fullname)
+        except IOError as e:
+            self.cache_msg = str(e)
+            raise CacheIOError(self.cache_msg)
+        return self.data
+            
+
 class CacheManager(object):
     """An object for maintaining caches"""
     def __init__(self,**kwargs):
-        super(CacheManager, self).__init__(**kwargs)
+        super(CacheManager, self).__init__()
         self.log = logging.getLogger(__name__)
         self.log.start()
         self.caches = {}
+        self.enabled = True
         
-    def register(self,name,generate=None,load=None,stage=None):
+    def registerNPY(self,name,generate=None,stage=None,directory=None,filename=None,**kwargs):
+        """Register a Numpy Cache"""
+        if generate == None:
+            raise CacheError("Cache %s must specify generate function" % name)
+        cache = NumpyCache(name=name,generate=generate,load=lambda : None,stage=stage,directory=directory, filename=filename, **kwargs)
+        self.caches[name] = cache
+        self.log.debug("Registered Cache %s" % name)
+    
+    def register(self,name,generate=None,load=None,stage=None,directory=None):
         """Register a new cache, which will then be managed by this cache manager"""
         if generate == None:
             raise CacheError("Cache %s must specify generate function" % name)
         if load == None:
             raise CacheError("Cache %s must specify load function" % name)
-        cache = Cache(name,generate,load,stage)
+        cache = Cache(name,generate,load,stage=stage,directory=directory)
         self.caches[name] = cache
         self.log.debug("Registered Cache %s" % name)
     
@@ -165,7 +209,7 @@ class CacheManager(object):
     
     def get(self,cache):
         """Get a particular cache"""
-        if self.shouldGenerate(cache):
+        if self.shouldGenerate(cache) or not ( enabled or self.caches[cache].generated ):
             self.generate(cache)
         return self.caches[cache]()
         
