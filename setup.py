ae69a41ad91c11ec96c3192f2b18aefb84986f48
Alex Rudy <alex.rudy@gmail.com>
2011-12-30 11:42:53 +0800

    Improved build and distribution now works with buildout

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-from setuptools import setup, find_packages
+from distribute_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
 setup(
     name = "AstroObject",
     version = "0.2.8",
-    py_modules=["AstroObject"],
-    packages = find_packages(
-        exclude=['tests.*'],
-        ),
-    package_data = {'':['VERSION','README.md','LICENCE']},
-    include_package_data = True,
-    exclude_package_data = {'': ['.gitignore','bump-version.sh','distribute.sh'], 'Docs/build':['*.rst']},
-    install_requires = ['pyfits>=2.4','numpy>=1.6','scipy>=0.9','matplotlib>=1.1'],
+    packages = ['AstroObject'],
+    package_data = {'':['VERSION']},
+    install_requires = ['distribute','pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0'],
     test_suite = 'tests',
 )
