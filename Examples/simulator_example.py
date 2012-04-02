8b859ebb23160c7411d70d038cbc030dd3f15b65
Alex Rudy <alex.rudy@gmail.com>
2012-04-02 18:52:30 +0800

    Example does minimal configuration construction

--- Examples/simulator_example.py
+++ Examples/simulator_example.py
@@ -24,26 +24,27 @@ class SimpleStage(Simulator):
     """An example simulator designed to show the power of simulations and for use while testing."""
     def __init__(self,*args,**kwargs):
         super(SimpleStage, self).__init__(*args,**kwargs)
-        self.registerStage(self.main,name="examp",description="Example Stage",include=True)
-        self.registerStage(self.other,name="other",description="Other Stage",include=True)
-        self.registerStage(self.last,name="last",description="Last Stage",include=True)
-        self.registerStage(None,"ex",dependencies=["examp","other"],help="example Macro")
+        self.registerStage(self.main,include=True)
+        self.registerStage(self.other,include=True)
+        self.registerStage(self.last,include=True)
+        self.registerStage(None,"ex",description="Example Macro",dependencies=["main","other"],help="example Macro")
         self.Caches["Random Image"] = Cache(self.cache,self.load,self.save)
         self.Caches["Random NPY"] = NumpyCache(self.cache,filename="%s/Random.npy" % self.config["Dirs"]["Caches"])
         
     def main(self):
+        """Main Stage"""
         print "Hello from %s Object" % self.name
         img = self.Caches["Random Image"]
         print img[0,0]
         
     def other(self):
-        """Other Stage Function"""
+        """Other Stage"""
         print "Hello from %s Stage" % "other"
         img = self.Caches["Random NPY"]
         print img[1,1]
     
     def last(self):
-        """Last Stage Function"""
+        """Last Stage"""
         print "Last Stage"
         img = self.Caches["Random Image"]
         print img[0,0]
