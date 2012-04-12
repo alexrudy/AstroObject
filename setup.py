3136dc9e822dcf52aa08c660badffc3e1e696f1d
Alex Rudy <alex.rudy@gmail.com>
2012-04-12 16:58:05 +0800

    Better setup support, and removing matplotlib requirement explicit

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
 from distribute_setup import use_setuptools
 use_setuptools()
 from setuptools import setup
+
 from distutils.command.build_py import build_py as du_build_py
+from distutils.core import Command
+
+from pkg_resources import parse_version
+
 from AstroObject.version import version as versionstr
+
+# Check for matplotlib:
+try:
+    from matplotlib import __version__ as mplversion
+    assert parse_version(mplversion) >= parse_version("1.0")
+except:
+    print "Matplotlib does not appear to be installed correctly.\nYou must install Matplotlib 1.0 or later."
+
+class Version(Command):
+    description = "Print the module version"
+    user_options = []
+    def initialize_options(self):
+        pass
+        
+    def finalize_options (self):
+        pass
+        
+    def run(self):
+        print 'version',versionstr
+        return
+
 
 #custom build_py overwrites version.py with a version overwriting the revno-generating version.py
 class ao_build_py(du_build_py):
@@ -34,11 +60,12 @@ setup(
     version = versionstr,
     packages = ['AstroObject'],
     package_data = {'':['Defaults.yaml']},
-    install_requires = ['distribute','pyfits>=2.4','numpy>=1.5','scipy>=0.9','matplotlib>=1.0','progressbar'],
+    install_requires = ['distribute','pyfits>=2.4','numpy>=1.5','scipy>=0.9',"PyYAML>=3.10",'progressbar'],
     test_suite = 'tests',
     author = "Alexander Rudy",
     author_email = "dev@alexrudy.org",
     cmdclass = {
         'build_py' : ao_build_py,
+        'version' : Version
     }
 )
