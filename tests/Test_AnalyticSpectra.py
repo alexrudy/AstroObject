ce868c0b9a1e899fb9eb40735cee0f08c845beb9
Alex Rudy <alex.rudy@gmail.com>
2012-04-17 01:30:13 +0800

    More comprehensive testing of InterpolatedSpectrum call methods

--- tests/Test_AnalyticSpectra.py
+++ tests/Test_AnalyticSpectra.py
@@ -23,6 +23,7 @@ from tests.Test_AstroObjectAPI import *
 from tests.Test_AstroSpectra import *
 
 import AstroObject.AnalyticSpectra as AS
+from AstroObject.Utilities import *
 
 class API_AnalyticSpectra(API_Abstract_Frame):
     """Set up and basic tests for analytic spectra"""
@@ -174,13 +175,32 @@ class test_InterpolatedSpectra(API_AnalyticSpectra,test_SpectraFrame):
         figure = AFrame.__show__()
         assert isinstance(figure,self.SHOWTYPE), "Found type %s" % type(figure)
     
+    def save_or_compare(self,data,filename,skip=True):
+        """Save or compare data"""
+        try:
+            old_data = np.load(filename)
+        except IOError:
+            np.save(filename,data)
+            if skip:
+                raise SkipTest
+            else:
+                return True
+        else:
+            passed = self.SAMEDATA(data,old_data)
+            
+            if not passed:
+                print npArrayInfo(data,"New Calc")
+                print npArrayInfo(old_data,"Old Data")
+            return passed
+    
     
     def test_call_resample(self):
-        """__call__(method='resample') yields data"""
+        """__call__(method='resample') yields valid data"""
         AFrame = self.FRAME(data=self.VALID,label="Valid")
         assert AFrame.label == "Valid"
         WL = self.WAVELENGHTS_LOWR
-        data = AFrame(wavelengths=WL[:-1],resolution=(WL[:-1]/np.diff(WL))/4,method='resample')
+        data = AFrame(wavelengths=WL[:-1],resolution=(WL[:-1]/np.diff(WL))/4,method='resample')        
+        assert self.save_or_compare(data,"tests/data/resample.npy")
         
     def test_call_resample_with_arbitrary_arguments(self):
         """__call__(method='resample') accepts arbitrary keyword arguments"""
@@ -188,17 +208,90 @@ class test_InterpolatedSpectra(API_AnalyticSpectra,test_SpectraFrame):
         assert AFrame.label == "Valid"
         WL = self.WAVELENGHTS_LOWR
         data = AFrame(wavelengths=WL[:-1],resolution=(WL[:-1]/np.diff(WL))/4,other=1,arbitrary="str",arguments="blah",method='resample')
-    
+        assert self.save_or_compare(data,"tests/data/resample2.npy",skip=False)
 
     def test_call_integrate_quad(self):
-        """__call__(method='integrate_quad') yields data"""
+        """__call__(method='integrate_quad') yields valid data"""
         AFrame = self.FRAME(data=self.VALID,label="Valid")
         assert AFrame.label == "Valid"
         data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=(self.WAVELENGTHS[:-1]/np.diff(self.WAVELENGTHS)),method="integrate_quad")
+        assert self.save_or_compare(data,"tests/data/integrateQ.npy")
+        
         
     def test_call_integrate_quad_with_arbitrary_arguments(self):
         """__call__(method='integrate_quad') accepts arbitrary keyword arguments"""
         AFrame = self.FRAME(data=self.VALID,label="Valid")
         assert AFrame.label == "Valid"
         data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),other=1,arbitrary="str",arguments="blah",method="integrate_quad")
+        assert self.save_or_compare(data,"tests/data/integrateQ2.npy",skip=False)
+        
+    def test_call_integrate_hist_with_arbitrary_arguments(self):
+        """__call__(method='integrate_hist')  accepts arbitrary keyword arguments"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),other=1,arbitrary="str",arguments="blah",method="integrate_hist")
+        assert self.save_or_compare(data,"tests/data/integrateH2.npy",skip=False)
+    
+    def test_call_integrate_hist(self):
+        """__call__(method='integrate_hist') yields valid data"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),method="integrate_hist")
+        assert self.save_or_compare(data,"tests/data/integrateH.npy")
+
+    def test_call_interpolate_with_arbitrary_arguments(self):
+        """__call__(method='interpolate')  accepts arbitrary keyword arguments"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),other=1,arbitrary="str",arguments="blah",method="interpolate")
+        assert self.save_or_compare(data,"tests/data/interpolate2.npy",skip=False)
+    
+    def test_call_interpolate(self):
+        """__call__(method='interpolate') yields valid data"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),method="interpolate")
+        assert self.save_or_compare(data,"tests/data/interpolate.npy")
+
+    def test_call_polyfit_with_arbitrary_arguments(self):
+        """__call__(method='polyfit')  accepts arbitrary keyword arguments"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),other=1,arbitrary="str",arguments="blah",method="polyfit")
+        assert self.save_or_compare(data,"tests/data/polyfit2.npy",skip=False)
+    
+    def test_call_polyfit(self):
+        """__call__(method='polyfit') yields valid data"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),method="polyfit")
+        assert self.save_or_compare(data,"tests/data/polyfit.npy")
+        
+    def test_call_resolve_with_arbitrary_arguments(self):
+        """__call__(method='resolve')  accepts arbitrary keyword arguments"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),other=1,arbitrary="str",arguments="blah",method="resolve")
+        assert self.save_or_compare(data,"tests/data/resolve2.npy",skip=False)
+    
+    def test_call_resolve(self):
+        """__call__(method='resolve') yields valid data"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),method="resolve")
+        assert self.save_or_compare(data,"tests/data/resolve.npy")
+
+    def test_call_resolve_and_integrate_arguments(self):
+        """__call__(method='resolve_and_integrate')  accepts arbitrary keyword arguments"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),other=1,arbitrary="str",arguments="blah",method="resolve_and_integrate")
+        assert self.save_or_compare(data,"tests/data/resolve_and_integrate2.npy",skip=False)
+    
+    def test_call_resolve_and_integrate(self):
+        """__call__(method='resolve_and_integrate') yields valid data"""
+        AFrame = self.FRAME(data=self.VALID,label="Valid")
+        assert AFrame.label == "Valid"
+        data = AFrame(wavelengths=self.WAVELENGTHS[:-1],resolution=np.diff(self.WAVELENGTHS),method="resolve_and_integrate")
+        assert self.save_or_compare(data,"tests/data/resolve_and_integrate.npy")
     
